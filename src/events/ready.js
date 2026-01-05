3535af20b5a14c5ddbf647d6d03b37ee09177cec
slophisticated <angeloramdani24@gmail.com>
2026-01-05 21:14:51 +0700

    update watching members

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,11 +5,35 @@ export default {
   once: true,
   execute(client) {
     console.log(`🚀 Bot is online as ${client.user.tag}`);
-    client.user.setPresence({
-      activities: [
-        { name: "Whitelist & MM System", type: ActivityType.Watching },
-      ],
-      status: "online",
-    });
+
+    // Update pertama saat bot nyala
+    const guild = client.guilds.cache.get("ID_SERVER_KAMU");
+    if (guild) {
+      client.user.setPresence({
+        activities: [
+          {
+            name: `${guild.memberCount} Members`,
+            type: ActivityType.Watching,
+          },
+        ],
+        status: "online",
+      });
+    }
+
+    // Update berkala tiap 10 menit
+    setInterval(() => {
+      const guild = client.guilds.cache.get("1422567690362683507");
+      if (!guild) return;
+
+      client.user.setPresence({
+        activities: [
+          {
+            name: `${guild.memberCount} Members`,
+            type: ActivityType.Watching,
+          },
+        ],
+        status: "online",
+      });
+    }, 600000);
   },
 };
